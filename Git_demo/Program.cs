9fa71b959b2225f8f4a3f7f8232b71ef2caba559
PhamTrang01 <tp5537029@gmail.com>
2021-10-07 16:44:54 +0700

    Add project files.

---
+++ Git_demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Git_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
